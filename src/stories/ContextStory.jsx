ade510ba20c2013ae83147e1968b20651933cabd
jo <jo>
2022-04-08 10:02:42 +0900

    6-1 컨텍스트의 기초 개념

---
+++ src/stories/ContextStory.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import HomePageComponent from '../06/HomePageComponent';
+import HomePageWithProvider from '../06/HomePageWithProvider';
+
+storiesOf('HomePageComponent', module)
+  .addWithJSX('컨텍스트 예제', () => <HomePageComponent />)
+  .addWithJSX('Provider 예제', () => <HomePageWithProvider />);
