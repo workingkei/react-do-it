760947ba55f214aac57e1246e2193a8bc7135b7e
jo <jo>
2022-04-15 10:09:56 +0900

    6-2 컨텍스트 제대로 사용하기

---
+++ src/06/ButtonWithConsumer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../04/Button';
+import createLoadingConsumer from './createLoadingConsumer';
+
+const DefaultLoadingConsumer = createLoadingConsumer();
+const Loading2Consumer = createLoadingConsumer('loading2');
+
+export default function ButtonWithConsumer({ children }) {
+  return (
+    <>
+      <DefaultLoadingConsumer
+        render={({ loading, setLoading }) => (
+          <Button onPress={() => setLoading(!loading)}>
+            {loading ? '컨텍스트1 로딩 중' : children}
+          </Button>
+        )}
+      />
+      <Loading2Consumer
+        render={({ loading, setLoading }) => (
+          <Button onPress={() => setLoading(!loading)}>
+            {loading ? '컨텍스트2 로딩 중' : children}
+          </Button>
+        )}
+      />
+      <DefaultLoadingConsumer
+        render={({ loading, setLoading }) => (
+          <Loading2Consumer
+            render={({ loading: loading2 }) => (
+              <Button onPress={() => setLoading(!loading)}>
+                {loading || loading2 ? '로딩중' : children}
+              </Button>
+            )}
+          />
+        )}
+      />
+    </>
+  );
+}
---
+++ src/06/ButtonWithLoadingContextAndKey.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../04/Button';
+import withLoadingContextAndKey, { loadingPropTypes } from './withLoadingContextAndKey';
+import PropTypes from 'prop-types';
+
+function ButtonWithLoadingContext({ children, loading, setLoading }) {
+  return <Button onPress={() => setLoading(!loading)}>{loading ? '로딩 중' : children}</Button>;
+}
+ButtonWithLoadingContext.propTypes = {
+  ...loadingPropTypes,
+  children: PropTypes.string,
+};
+
+export const ButtonWithDefaultLoadingContext = withLoadingContextAndKey()(ButtonWithLoadingContext);
+export const ButtonWithLoading2Context =
+  withLoadingContextAndKey('loading2')(ButtonWithLoadingContext);
