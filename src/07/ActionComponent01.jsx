2c32d50780592ab52413fffc554b44ed9a08396a
jo <jo>
2022-05-04 14:30:01 +0900

    7-4 데이터를 위한 컴포넌트 알아보기

---
+++ src/07/ActionComponent01.jsx
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../04/Button';
+
+class ActionComponent extends PureComponent {
+  render() {
+    const { setAge } = this.props;
+
+    return (
+      <>
+        <Button onPress={() => setAge(2, 19)}>고유번호 2번 나이 19세로 변경</Button>
+      </>
+    );
+  }
+}
+
+ActionComponent.propTypes = {
+  setAge: PropTypes.func,
+};
+export default ActionComponent;
