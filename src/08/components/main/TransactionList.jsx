cd9f0c0e44c85ba47582176bfe6d401429974db3
jo <jo>
2022-05-09 14:07:57 +0900

    9-1 가상 데이터 서버 설정하기, 9-2 데이터 요청을 위한 axios 라이브러리 도입하기

--- src/08/components/main/TransactionList.jsx
+++ src/08/components/main/TransactionList.jsx
@@ -3,19 +3,26 @@ import Heading from '../../../doit-ui/Heading';
 import Card from '../../../doit-ui/Card';
 import TransactionSearchFilter from './TransactionSearchFilter';
 import TransactionTable from './TransactionTable';
+import Api from '../../Api';
 
 class TransactionList extends PureComponent {
   state = {
     transactions: [
-      {
-        id: 'btx_01',
-        name: '비트코인(BTX)',
-        totalPrice: '123,123,000,000원',
-        currentPrice: '4,200,000원',
-        datetime: '2019/01/20 08:23:22',
-      },
+      // {
+      //   id: 'btx_01',
+      //   name: '비트코인(BTX)',
+      //   totalPrice: '123,123,000,000원',
+      //   currentPrice: '4,200,000원',
+      //   datetime: '2019/01/20 08:23:22',
+      // },
     ],
   };
+
+  componentDidMount() {
+    Api.get('/transactions', { params: { code: 'BTX' } }).then((response) =>
+      this.setState({ transactions: response.data }),
+    );
+  }
 
   render() {
     const { transactions } = this.state;
