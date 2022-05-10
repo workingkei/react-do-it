5d23d32c4533c8b8fad143401bd565128405a4b3
jo <jo>
2022-05-10 11:12:25 +0900

    10-1 미들웨어 기초 알아보기

--- src/08/store/configureStore.js
+++ src/08/store/configureStore.js
@@ -1,6 +1,53 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import reducers from '../reducers';
+// import { SET_TRANSACTION_LIST } from '../actions/transactionActions';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const customMiddleware = (store) => (nextRunner) => (action) => {
+//   console.log('액션 객체', action);
+//   console.log('리듀서 실행 전', store.getState());
+//   const result = nextRunner(action);
+//   console.log('리듀서 실행 후', store.getState());
+//   return result;
+// };
+const customMiddleware = (store) => (nextRunner) => (action) => {
+  console.log('미들웨어에 전달된 액션 객체', action);
+  console.log('리듀서 실행 전', store.getState());
+  const result = nextRunner(action);
+  console.log('리듀서 실행 후', store.getState());
+  return result;
+};
+
+const customMiddleware2 = (store) => (nextRunner) => (action) => {
+  console.log('미들웨어2에 전달된 액션 객체', action);
+  console.log('미들웨어2 실행 전', store.getState());
+  const result = nextRunner(action);
+  console.log('미들웨어2 실행 후', store.getState());
+  return result;
+};
+
+// const customMiddleware1 = () => (nextRunner) => (action) => {
+//   if (action.type === SET_TRANSACTION_LIST) {
+//     return nextRunner({
+//       ...action,
+//       payload: [
+//         {
+//           id: 0,
+//           code: 'DOIT',
+//           name: '두잇코인(DOIT)',
+//           totalPrice: 100000000,
+//           currentPrice: 25000,
+//           datetime: '현재시간',
+//         },
+//       ],
+//     });
+//   }
+//   return nextRunner(action);
+// };
 
 export default (initStates) =>
-  createStore(combineReducers(reducers), initStates, composeWithDevTools());
+  createStore(
+    combineReducers(reducers),
+    initStates,
+    composeWithDevTools(applyMiddleware(customMiddleware, customMiddleware2)),
+  );
